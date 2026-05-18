a3ef2479ecd0ab72c094077b09dd5478d742ca41
envikaaa <envikapriyatna31@gmail.com>
2026-05-18 14:46:28 +0700

    menambahkan int main

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -27,4 +27,18 @@ public:
     {
         cout << "pelajar dibuat\n" << endl;
     }
-}
+};
+
+class budi : public pekerja, public pelajar {
+public:
+
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur) //hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+
+int main()
